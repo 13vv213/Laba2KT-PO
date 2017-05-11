9f8d1dfcd5cc4af06d932fe28b658fbd3ad7163c
хз <@DESKTOP-MRTV5PC>
2017-05-11 09:39:25 +0300

    Добавьте файлы проекта.

---
+++ laba2KTPO/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace laba2KTPO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 102);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(343, 261);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 36);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(41, 20);
+            this.numericUpDown1.TabIndex = 1;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(13, 62);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDown2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Создать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 369);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Преобразовать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(372, 102);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(343, 261);
+            this.dataGridView2.TabIndex = 5;
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(592, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Чтение из файла";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Введите размерность матрицы";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(592, 73);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(123, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Сохранить в файл";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 446);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ laba2KTPO/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba2KTPO
+{
+    public partial class Form1 : Form
+    {
+        bool q = false;
+        bool z = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (numericUpDown1.Value > 100 || numericUpDown2.Value > 100)
+            {
+                MessageBox.Show("Вводите числа от 1 до 100", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                numericUpDown1.Value = 0;
+                numericUpDown2.Value = 0;
+            }
+            else
+            {
+                dataGridView1.DataSource = null;
+                dataGridView1.ColumnCount = 0;
+                dataGridView1.RowCount = 1;
+
+                dataGridView2.ColumnCount = 0;
+
+                dataGridView1.ColumnCount = Convert.ToInt32(numericUpDown1.Value);
+                dataGridView1.RowCount = Convert.ToInt32(numericUpDown2.Value);
+
+                q = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            z = false;
+            try
+            {
+
+                try
+                {
+
+                    for (int i = 0; i < dataGridView1.ColumnCount; i++)
+                    {
+                        for (int j = 0; j < dataGridView1.RowCount; j++)
+                        {
+                            if (Convert.ToInt32(dataGridView1[i, j].Value) > 2147483647 || Convert.ToInt32(dataGridView1[i, j].Value) < -2147483648)
+                            {
+
+                            }
+                        }
+                    }
+                }
+                catch (OverflowException)
+                {
+                    MessageBox.Show("Элементы матрицы должны быть в диапазоне от -2147483648 до 2147483647", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    z = true;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Элементы матрицы должны быть только цифрами", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                z = true;
+            }
+            if (z == false)
+            {
+                if (numericUpDown1.Value != 0 && numericUpDown2.Value != 0 && q == true)
+                {
+                    dataGridView2.ColumnCount = Convert.ToInt32(numericUpDown1.Value);
+                    dataGridView2.RowCount = Convert.ToInt32(numericUpDown2.Value);
+
+                }
+                else
+                {
+                    dataGridView2.ColumnCount = dataGridView1.ColumnCount;
+                    dataGridView2.RowCount = dataGridView1.RowCount;
+                }
+                if (numericUpDown1.Value != 0 && numericUpDown2.Value != 0 && q == true)
+                {
+                    for (int i = 0; i < numericUpDown1.Value; i++)
+                    {
+                        for (int j = 0; j < numericUpDown2.Value; j++)
+                            if (i % 2 == 0)
+                                dataGridView2[i, j].Value = dataGridView1[i, j].Value;
+                            else
+                            {
+                                for (int h = Convert.ToInt32(numericUpDown2.Value) - 1; h >= 0; h--)
+                                    dataGridView2[i, Convert.ToInt32(numericUpDown2.Value) - 1 - h].Value = dataGridView1[i, h].Value;
+                            }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < dataGridView1.ColumnCount; i++)
+                    {
+                        for (int j = 0; j < dataGridView1.RowCount; j++)
+                            if (i % 2 == 0)
+                                dataGridView2[i, j].Value = dataGridView1[i, j].Value;
+                            else
+                            {
+                                for (int h = dataGridView1.RowCount - 1; h >= 0; h--)
+                                    dataGridView2[i, dataGridView1.RowCount - 1 - h].Value = dataGridView1[i, h].Value;
+                            }
+                    }
+                }
+            }
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            q = false;
+            dataGridView1.DataSource = null;
+            dataGridView1.ColumnCount = 0;
+
+            dataGridView2.ColumnCount = 0;
+
+            OpenFileDialog gg = new OpenFileDialog();
+            if (gg.ShowDialog() == DialogResult.OK)
+            {
+                DataSet ds = new DataSet();
+                ds.Tables.Add("score");
+                string qwe = gg.FileName;
+                StreamReader pgg = new StreamReader(qwe);
+                string head = pgg.ReadLine();
+                if (head == null)
+                {
+                    MessageBox.Show("Пустой файл!", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+                else
+                {
+                    string[] col = System.Text.RegularExpressions.Regex.Split(head, " ");
+                for (int i = 0; i < col.Length; i++)
+                {
+                    ds.Tables[0].Columns.Add();
+                }
+                StreamReader omgsyka = new StreamReader(qwe);
+                string row = omgsyka.ReadLine();
+                    while (row != null)
+                    {
+                        string[] rvalue = System.Text.RegularExpressions.Regex.Split(row, " ");
+                        ds.Tables[0].Rows.Add(rvalue);
+                        row = pgg.ReadLine();
+                    }
+                    if (ds.Tables[0].Columns.Count > 100 || ds.Tables[0].Rows.Count > 100)
+                    {
+                        MessageBox.Show("Размеры превышают допустимые(100х100)", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        dataGridView1.DataSource = ds.Tables[0];
+                    }
+                }
+            }
+            
+       }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog save = new OpenFileDialog();
+            save.Filter = "Text|*.txt|All|*.*";
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                using(StreamWriter sav = new StreamWriter(save.FileName, false, System.Text.Encoding.Default))
+                    for (int i = 0; i < dataGridView2.RowCount; i++)
+                    {
+                        if(i!=0)
+                            sav.WriteLine();
+                        for (int j = 0; j < dataGridView2.ColumnCount; j++)
+                        {
+                           sav.Write(Convert.ToString(dataGridView2[j, i].Value) + " ");
+                        }
+                    }
+            }
+        }
+    }
+}
